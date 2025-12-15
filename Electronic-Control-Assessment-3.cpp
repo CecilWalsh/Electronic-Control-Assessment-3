3206593df22e6e9badeafaf851fdcd0da6a02eda
SHM_white <1131287310a@gmail.com>
2025-12-15 19:18:39 +0800

    添加项目文件。

---
+++ Electronic-Control-Assessment-3.cpp
@@ -0,0 +1,12 @@
+﻿// Electronic-Control-Assessment-3.cpp: 定义应用程序的入口点。
+//
+
+#include "Electronic-Control-Assessment-3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
